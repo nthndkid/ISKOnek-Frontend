88c3b8e7d104247c3c245825e507ae26cb8568f0
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-07-24 01:52:36 +0800

    improved navbar and responsive, added logout functionality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,106 @@
-<template>
-  <v-container>
-    <v-app-bar app color="white" class="px-10 py-2" dark>
-      <!-- Logo Left -->
-      <v-toolbar-title class="text-h6 font-weight-bold">
-        <img :src="Logo" alt="Logo" height="32" class="mr-2" />
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <!-- Nav Links -->
-      <v-btn text to="/">Home</v-btn>
-      <v-btn text to="/pre-match">Match</v-btn>
-
-      <!-- Profile Dropdown -->
-      <v-menu>
-        <template #activator="{ props }">
-          <v-btn v-bind="props" icon>
-            <v-avatar size="32">
-              <img src="https://randomuser.me/api/portraits/men/85.jpg" alt="Profile" />
-            </v-avatar>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item @click="logout">
-            <v-list-item-title>Logout</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-  </v-container>
-</template>
-
 <script setup>
 import { useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import { useUserStore } from '../stores/userStore'
+import { useAvatarStore } from '../stores/avatarStore'
 import Logo from '../components/icons/iskonek.svg'
 
 const router = useRouter()
 
-function logout() {
-  // Simulate logout
-  alert('Logged out!')
-  router.push('/login')
+const userStore = useUserStore()
+const avatarStore = useAvatarStore()
+
+const logout = () =>  {
+  userStore.clearUser()
+  router.push('/')
 }
+
+onMounted(() => {
+  userStore.loadUser()
+  avatarStore.loadAvatar()
+})
+
+const user = ref({
+  name: 'John Doe',
+})
 </script>
 
+<template>
+  <v-app-bar app color="white" class="py-2"   elevate-on-scroll>
+    <v-container class="d-flex align-center justify-space-between">
+      <!-- Logo -->
+      <v-toolbar-title class="text-h6 mt-3 font-weight-bold d-flex align-center">
+        <img :src="Logo" alt="Logo" height="42" class="mr-2" />
+      </v-toolbar-title>
+
+      <!-- Desktop Menu (md and up) -->
+      <div class="d-none d-md-flex align-center gap-4">
+        <v-btn text to="/home" size="large" class="text-capitalize">Home</v-btn>
+        <v-btn text to="/pre-match" size="large" class="text-capitalize">Match</v-btn>
+
+        <v-menu offset-y>
+          <template #activator="{ props }">
+            <v-btn v-bind="props" class="d-flex align-center text-none" variant="text">
+              <v-avatar size="32" class="mr-2">
+                <img :src="avatarStore.avatar" alt="Avatar" />
+              </v-avatar>
+              <!-- <span class="text-body-1 font-weight-medium">{{ user.name }}</span> -->
+              <v-icon end>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item>
+              <div class="d-flex align-center">
+                <v-icon class="mr-2">mdi-account</v-icon>
+                <v-list-item-title class="font-weight-medium">{{ userStore.user.username }}</v-list-item-title>
+              </div>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-item @click="logout">
+              <div class="d-flex align-center">
+                <v-icon class="mr-2" color="red-accent-4">mdi-logout</v-icon>
+                <v-list-item-title>Logout</v-list-item-title>
+              </div>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
+      <!-- Mobile Hamburger (sm only) -->
+      <div class="d-md-none">
+        <v-menu offset-y>
+          <template #activator="{ props }">
+            <v-btn icon v-bind="props">
+              <v-icon>mdi-menu</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item to="/">
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/pre-match">
+              <v-list-item-title>Match</v-list-item-title>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list-item>
+              <v-list-item-title class="font-weight-medium">{{ user.name }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logout">
+              <v-icon>mdi-account</v-icon>
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+
 <style scoped>
-/* Optional: make logo responsive */
 img {
-  max-height: 32px;
+  max-height: 42px;
 }
 </style>
